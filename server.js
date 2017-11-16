1de458290f06fdd750189d090589045179a0ff8c
Rosie Armstrong <rosemary_a@live.co.uk>
2017-11-16 20:10:16 +0000

    initial set-up including seed data

---
+++ server.js
@@ -0,0 +1,23 @@
+if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
+
+const express = require('express'); 
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const config = require('./config');
+const router = require('./routes');
+
+const db = config.DB[process.env.NODE_ENV] || process.env.DB;
+
+mongoose.Promise = Promise;
+
+mongoose.connect(db, { useMongoClient: true })
+  .then(() => console.log('successfully connected to', db))
+  .catch(err => console.log('connection failed', err));
+
+app.use(bodyParser.json());
+
+app.use('/api', router);
+
+
+module.exports = app;
